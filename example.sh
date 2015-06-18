41d9488944f30ac61223a4ca33378f20ff5aace5
rbong <roger.bongers@yahoo.ca>
2015-06-18 19:51:57 -0400

    Improved script.

--- example.sh
+++ example.sh
@@ -1,74 +1,22 @@
 #!/bin/bash
 arrowkey | while read -r -n 1 c; do
-d=$(LC_TYPE=C printf '%d\n' "'$c")
-if [ "$d" == "8" ]; then
-    c="BackSpace"
-elif [ "$d" == "0" ]; then
-    c="space"
-elif [ "$c" == "\\" ]; then
-    c="backslash"
-elif [ "$c" == "!" ]; then
-    c="exclam"
-elif [ "$c" == "~" ]; then
-    c="asciitilde"
-elif [ "$c" == "\`" ]; then
-    c="quoteleft"
-elif [ "$c" == "@" ]; then
-    c="ampersand"
-elif [ "$c" == "#" ]; then
-    c="numbersign"
-elif [ "$c" == "$" ]; then
-    c="dollar"
-elif [ "$c" == "%" ]; then
-    c="percent"
-elif [ "$c" == "^" ]; then
-    c="asciicircum"
-elif [ "$c" == "&" ]; then
-    c="ampersand"
-elif [ "$c" == "*" ]; then
-    c="asterisk"
-elif [ "$c" == "(" ]; then
-    c="parenleft"
-elif [ "$c" == ")" ]; then
-    c="parenright"
-elif [ "$c" == "-" ]; then
-    c="minus"
-elif [ "$c" == "=" ]; then
-    c="equal"
-elif [ "$c" == "_" ]; then
-    c="underscore"
-elif [ "$c" == "+" ]; then
-    c="plus"
-elif [ "$c" == "[" ]; then
-    c="bracketleft"
-elif [ "$c" == "]" ]; then
-    c="bracketright"
-elif [ "$c" == "{" ]; then
-    c="braceleft"
-elif [ "$c" == "}" ]; then
-    c="braceright"
-elif [ "$c" == "|" ]; then
-    c="bar"
-elif [ "$c" == ";" ]; then
-    c="semicolon"
-elif [ "$c" == "'" ]; then
-    c="quoteright"
-elif [ "$c" == ":" ]; then
-    c="colon"
-elif [ "$c" == "\"" ]; then
-    c="quotedbl"
-elif [ "$c" == "," ]; then
-    c="comma"
-elif [ "$c" == "." ]; then
-    c="period"
-elif [ "$c" == "/" ]; then
-    c="slash"
-elif [ "$c" == "<" ]; then
-    c="less"
-elif [ "$c" == ">" ]; then
-    c="greater"
-elif [ "$c" == "?" ]; then
-    c="question"
-fi
-    xdotool search Firefox keydown "$c" keyup "$c"
+    d=$(LC_TYPE=C printf '%d\n' "'$c")
+    declare -A keys=( ["\\"]="backslash" ["!"]="exclam" ["~"]="asciitilde"
+        ["\`"]="quoteleft" ["@"]="ampersand" ["#"]="numbersign" ["$"]="dollar"
+        ["%"]="percent" ["^"]="asciicircum" ["&"]="ampersand" ["*"]="asterisk"
+        ["("]="parenleft" [")"]="parenright" ["-"]="minus" ["="]="equal"
+        ["_"]="underscore" ["+"]="plus" ["["]="bracketleft"
+        ["]"]="bracketright" ["{"]="braceleft" ["}"]="braceright" ["|"]="bar"
+        [";"]="semicolon" ["'"]="quoteright" [":"]="colon" ["\""]="quotedbl"
+        [","]="comma" ["."]="period" ["/"]="slash" ["<"]="less" [">"]="greater"
+        ["?"]="question" )
+    if [ "$d" == "8" ]; then
+        c="BackSpace"
+    elif [ "$d" == "0" ]; then
+        c="space"
+    elif [ "${keys[$c]}" != "" ]; then
+        c="${keys[$c]}"
+    fi
+    echo "$c"
+    # xdotool search Firefox keydown "$c" keyup "$c"
 done
