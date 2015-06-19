1ee4e1fffc24744a0a5473e9bb15bdbd429ee378
rbong <roger.bongers@yahoo.ca>
2015-06-19 02:12:27 -0400

    Fixed example.sh for older versions of Bash.

--- example.sh
+++ example.sh
@@ -2,21 +2,21 @@
 arrowkey | while read -r -n 1 c; do
     d=$(LC_TYPE=C printf '%d\n' "'$c")
     # this lookup array indexes special keys for X
-    declare -A keys=( ["\\"]="backslash" ["!"]="exclam" ["~"]="asciitilde"
-        ["\`"]="quoteleft" ["@"]="ampersand" ["#"]="numbersign" ["$"]="dollar"
-        ["%"]="percent" ["^"]="asciicircum" ["&"]="ampersand" ["*"]="asterisk"
-        ["("]="parenleft" [")"]="parenright" ["-"]="minus" ["="]="equal"
-        ["_"]="underscore" ["+"]="plus" ["["]="bracketleft"
-        ["]"]="bracketright" ["{"]="braceleft" ["}"]="braceright" ["|"]="bar"
-        [";"]="semicolon" ["'"]="quoteright" [":"]="colon" ["\""]="quotedbl"
-        [","]="comma" ["."]="period" ["/"]="slash" ["<"]="less" [">"]="greater"
-        ["?"]="question" )
+    declare -A keys=( ["a\\"]="backslash" ["a!"]="exclam" ["a~"]="asciitilde"
+        ["a\`"]="quoteleft" ["a@"]="at" ["a#"]="numbersign" ["a$"]="dollar"
+        ["a%"]="percent" ["a^"]="asciicircum" ["a&"]="ampersand" ["a*"]="asterisk"
+        ["a("]="parenleft" ["a)"]="parenright" ["a-"]="minus" ["a="]="equal"
+        ["a_"]="underscore" ["a+"]="plus" ["a["]="bracketleft"
+        ["a]"]="bracketright" ["a{"]="braceleft" ["a}"]="braceright" ["a|"]="bar"
+        ["a;"]="semicolon" ["a'"]="quoteright" ["a:"]="colon" ["a\""]="quotedbl"
+        ["a,"]="comma" ["a."]="period" ["a/"]="slash" ["a<"]="less" ["a>"]="greater"
+        ["a?"]="question" )
     if [ "$d" == "8" ]; then
         c="BackSpace"
     elif [ "$d" == "0" ]; then
         c="space"
-    elif [ "${keys[$c]}" != "" ]; then
-        c="${keys[$c]}"
+    elif [ "${keys[a$c]}" != "" ]; then
+        c="${keys[a$c]}"
     fi
     xdotool search Firefox keydown "$c" keyup "$c"
 done
